a7b78261d31787fcc2f578a3744e0900d5f5ae79
Philip Jurke <p.jurke@telekom.de>
2019-11-27 18:27:43 +0100

    +date

---
+++ Date.Model.cs
@@ -0,0 +1,24 @@
+﻿namespace DataGraph.TimeModule
+{
+    public class Date
+    {
+
+        // Properties
+
+        public int? Day { get; set; }
+        public int? Month { get; set; }
+        public long? Year { get; set; }
+
+        // Constructors
+
+        public Date() { }
+
+        public Date(int? day, int? month, long? year)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+
+    }
+}
