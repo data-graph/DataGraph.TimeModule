ab318a2dd622c6397b7d2a416c6a5cc246f02e0b
Philip Jurke <p.jurke@telekom.de>
2019-11-27 18:45:12 +0100

    +Date validator

---
+++ DateValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace DataGraph.TimeModule
+{
+    public class DateValidator
+    {
+
+        public static void ValidateDate(Date date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void ValidateDay(int day)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void ValidateMonth(int month)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void ValidateYear(long year)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
